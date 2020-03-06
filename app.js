52570cec5d40ccf6ce2ae870f962e473cf591528
balajipachai <balaji.pachai08@gmail.com>
2020-03-06 16:35:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,27 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+require('dotenv').config();
+
+const utils = require('./utils/');
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+
+// For Geth
+const helloWorldJson = require('./build/contracts/HelloWorld.json');
+
+const HelloWorldContract = utils.getContractFromJSON(helloWorldJson);
+const helloWorldInstance = utils.initialSetup(HelloWorldContract, true, process.env.ENVIRONMENT);
+
+// Sets the watch on event
+const watchParams = {
+  contractInstance: helloWorldInstance,
+  eventName: 'LogMessageChanged',
+};
+utils.setWatchOnEvents(watchParams);
 
 app.use(logger('dev'));
 app.use(express.json());
