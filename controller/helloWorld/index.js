27f3dda0df6095ee92de2c2d1be9356f21c53790
balajipachai <balaji.pachai08@gmail.com>
2020-03-06 16:34:46 +0530

    Create index.js

---
+++ controller/helloWorld/index.js
@@ -0,0 +1,99 @@
+const utils = require('../../utils/');
+const helloWorldJson = require('../../build/contracts/HelloWorld.json');
+
+const HelloWorldContract = utils.getContractFromJSON(helloWorldJson);
+
+const callChangeMessage = async (params) => {
+  try {
+    const contractInstance = utils.initialSetup(HelloWorldContract);
+    const { message } = params;
+    const timestamp = Date.now();
+    const receipt = await contractInstance.methods.changeMessage(
+      message,
+      timestamp,
+    ).send({ from: process.env.OWNER });
+    return receipt;
+  } catch (e) {
+    throw new Error(e);
+  }
+};
+
+const getCurrentMessage = async () => {
+  try {
+    const contractInstance = utils.initialSetup(HelloWorldContract);
+    const message = await contractInstance.methods.message().call();
+    return message;
+  } catch (e) {
+    throw new Error(e);
+  }
+};
+
+const getGreetedHelloTo = async () => {
+  try {
+    const contractInstance = utils.initialSetup(HelloWorldContract);
+    const message = await contractInstance.methods.greetedHelloTo().call();
+    return message;
+  } catch (e) {
+    throw new Error(e);
+  }
+};
+
+const getContractOwner = async () => {
+  try {
+    const contractInstance = utils.initialSetup(HelloWorldContract);
+    const message = await contractInstance.methods.owner().call();
+    return message;
+  } catch (e) {
+    throw new Error(e);
+  }
+};
+
+const getAllEvents = async () => {
+  try {
+    const filterOptions = {
+      fromBlock: '0',
+      toBlock: 'latest',
+    };
+    const eventDetails = [];
+    const isSocketInstance = true;
+    const contractInstance = utils.initialSetup(HelloWorldContract, isSocketInstance);
+    await contractInstance.getPastEvents('LogMessageChanged', filterOptions, async (err, events) => {
+      events.forEach((event) => {
+        eventDetails.push({
+          event: event.event,
+          blockHash: event.blockHash,
+          blockNumber: event.blockNumber,
+          transactionHash: event.transactionHash,
+          oldMessage: event.returnValues.oldMessage,
+          newMessage: event.returnValues.newMessage,
+          timestamp: event.returnValues.timestamp,
+        });
+      });
+    });
+
+    await contractInstance.getPastEvents('LogNameChanged', filterOptions, async (err, events) => {
+      events.forEach((event) => {
+        eventDetails.push({
+          event: event.event,
+          blockHash: event.blockHash,
+          blockNumber: event.blockNumber,
+          transactionHash: event.transactionHash,
+          oldName: event.returnValues.oldName,
+          newName: event.returnValues.newName,
+          timestamp: event.returnValues.timestamp,
+        });
+      });
+    });
+    return eventDetails;
+  } catch (e) {
+    throw new Error(e);
+  }
+};
+
+module.exports = {
+  callChangeMessage,
+  getCurrentMessage,
+  getGreetedHelloTo,
+  getContractOwner,
+  getAllEvents,
+};
